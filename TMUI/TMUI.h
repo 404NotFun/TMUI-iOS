a969944125f62f87efd64ca91952864b21dd55a1
Jason Tsai <funnot404@gmail.com>
2017-04-24 20:49:58 +0800

    Initial Commit

---
+++ TMUI/TMUI.h
@@ -0,0 +1,19 @@
+//
+//  TMUI.h
+//  TMUI
+//
+//  Created by Jason Tsai on 2017/4/24.
+//  Copyright © 2017年 YomStudio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TMUI.
+FOUNDATION_EXPORT double TMUIVersionNumber;
+
+//! Project version string for TMUI.
+FOUNDATION_EXPORT const unsigned char TMUIVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TMUI/PublicHeader.h>
+
+
---
+++ TMUITests/TMUITests.swift
@@ -0,0 +1,36 @@
+//
+//  TMUITests.swift
+//  TMUITests
+//
+//  Created by Jason Tsai on 2017/4/24.
+//  Copyright © 2017年 YomStudio. All rights reserved.
+//
+
+import XCTest
+@testable import TMUI
+
+class TMUITests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
